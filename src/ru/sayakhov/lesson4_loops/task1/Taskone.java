2b694e8b39daa26f5f0a1d4847453573883b2620
eduard-sd <pr-spektr@yandex.ru>
2018-08-25 02:22:50 +0300

    Закончил первое задание найти мин и макс число

--- src/ru/sayakhov/lesson4_loops/task1/Taskone.java
+++ src/ru/sayakhov/lesson4_loops/task1/Taskone.java
@@ -1,11 +1,28 @@
 package ru.sayakhov.lesson4_loops.task1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Taskone {
+
     public static void main(String[] args) {
-/*          ● Написать программу для поиска минимального из 4 чисел введеных пользователем
-            ● Написать программу, которая описывает введенное число. Отрицательное оно число или положительное (или нулевое), чётное или
-        нечётное. Словами
-            ● Написать программу для вывода на экран таблицы умножения.
-            ● Написать программу, которая выводит арифметическую или геометрическую прогрессию для N чисел.*/
+        int i = 0;
+        int[] intArray = new int[5];
+        while(i<5){
+            System.out.print("Введите число ");
+            System.out.println(1+i);
+            Scanner scanner = new Scanner(System.in);
+            while(!scanner.hasNextInt()){ //проверка не равно числу
+                System.out.println("Пожалуйста введите только целые числа, без букв и знаков препинания.");
+                scanner.next();
+            }
+            int number = scanner.nextInt();
+            intArray[i++] = number;
+        }
+
+        Arrays.sort(intArray);
+        System.out.println("Minimum = " + intArray[0]);
+        System.out.println("Maximum = " + intArray[intArray.length-1]);
+
     }
 }
